bec7870f92270d69585a11addb8dcb50e8bbd539
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-08-15 23:23:30 +0200

    Added footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class="bg-dark text-white">
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-3 text-center">
+                <h5 class="text-uppercase mb-4">Resources</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2">
+                        <a href="https://laravel.com/" target="_blank" class="text-white">Laravel 11</a>
+                    </li>
+                    <li>
+                        <a href="https://getbootstrap.com" target="_blank" class="text-white">Bootstrap 5.3.3</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 text-center">
+                <h5 class="text-uppercase mb-4">Legal</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2">
+                        <a href="https://realrentcar.galdi.dev/privacy_policy" class="text-white">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="https://realrentcar.galdi.dev/terms_conditions" class="text-white">Terms & Conditions</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <hr class="my-4 border-gray" />
+
+        <div class="d-flex justify-content-between align-items-center">
+            <span>© 2024 All Rights Reserved by Cmiljaaa</span>
+            <div class="d-flex gap-4">
+                <a href="https://github.com/Cmiljaa/Polovni-Automobili" target="_blank" class="text-white">
+                    <i class="fab fa-github fa-2x"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
