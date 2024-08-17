0b5168a1a63131da59341422796225ff511dfb20
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-08-17 17:53:20 +0200

    Added UserController

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CarController::class, 'index']);
+Route::get('/login', [UserController::class, 'login'])
+->name('user.login'); 
 
 Route::resource('cars', CarController::class);
+
+Route::resource('user', UserController::class);
