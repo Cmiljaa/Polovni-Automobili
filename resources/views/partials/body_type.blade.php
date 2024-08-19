bbd8afa154d4d7b004779810bfd53820d1edfee0
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-08-19 22:46:12 +0200

    Sorted brands and body types

--- resources/views/partials/body_type.blade.php
+++ resources/views/partials/body_type.blade.php
@@ -1,14 +1,14 @@
 <label for="body_type" class="form-label">Body Type</label>
 <select id="body_type" name="body_type" class="form-select">
     <option value="">All Body Types</option>
-    <option value="sedan">Sedan</option>
-    <option value="hatchback">Hatchback</option>
-    <option value="suv">SUV</option>
-    <option value="coupe">Coupe</option>
+    <option value="crossover">Crossover</option>
     <option value="convertible">Convertible</option>
-    <option value="wagon">Wagon</option>
+    <option value="coupe">Coupe</option>
+    <option value="hatchback">Hatchback</option>
+    <option value="sedan">Sedan</option>
+    <option value="sports_car">Sports Car</option>
+    <option value="suv">SUV</option>
     <option value="truck">Truck</option>
     <option value="van">Van</option>
-    <option value="crossover">Crossover</option>
-    <option value="sports_car">Sports Car</option>
+    <option value="wagon">Wagon</option>
 </select>
