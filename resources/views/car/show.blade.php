e1c7a2bde8abd5a03001a06fd3140a7ef84cae12
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-08-23 16:12:17 +0200

    Added modal and icons

--- resources/views/car/show.blade.php
+++ resources/views/car/show.blade.php
@@ -2,8 +2,9 @@
 @section('content')
     <div class="basic-container" style="max-width: 80%; border: 0px; margin-top: 20px;">
         <div class="row">
-            <div class="col-md-6">
-                <img src="{{ asset($car->image) }}" alt="{{ $car->image }}" class="img-fluid" style="max-width: 100%;">
+            <div class="col-md-6 position-relative">
+                <img src="{{ asset($car->image) }}" alt="{{ $car->image }}" class="img-fluid zoom-icon" style="max-width: 100%;" data-bs-toggle="modal" data-bs-target=".bd-example-modal-lg">
+                <i class="fa fa-search-plus zoom-icon-overlay"></i>
             </div>
             <div class="col-md-6" style="font-size: 18px">
                 <h3>{{ $car->brand }}</h3>
@@ -12,6 +13,19 @@
                 <p><strong>Fuel:</strong> {{ Str::ucfirst($car->fuel) }}</p>
                 <p><strong>Body Type:</strong> {{ Str::ucfirst($car->body_type) }}</p>
                 <p><strong>Phone:</strong> {{ $car->user->phone }}</p>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade bd-example-modal-lg" tabindex="-1" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="myLargeModalLabel">{{$car->brand}}  {{$car->model}}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img src="{{ asset($car->image) }}" alt="{{ $car->image }}" class="img-fluid">
+                </div>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>@yield('title')</title>
+    <title>Polovni Automobili</title>
+    <link rel="icon" href="{{asset('icons/car-svgrepo-com.svg')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         body {
@@ -81,6 +82,25 @@
             margin: 0px auto;
             margin-top: 10px;
             max-width: 1100px;
+        }
+        .position-relative {
+            position: relative;
+        }
+
+        .zoom-icon-overlay {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 2.5rem;
+            color: rgba(255, 255, 255, 0.8);
+            opacity: 0;
+            transition: opacity 0.3s ease;
+            pointer-events: none;
+        }
+
+        .zoom-icon:hover + .zoom-icon-overlay {
+            opacity: 1;
         }
     </style>
 </head>
