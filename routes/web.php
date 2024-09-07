8c2f8fc2e2f0320b7d5a5d93e6e3ced58f0d3a10
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-09-07 17:40:14 +0200

    Fixed routes

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/cars');
 
-Route::get('/legal/privacy_policy', function(){
-    return view('partials.privacy_policy');
-})->name('legal.privacy_policy');
+Route::view('/privacy_policy', 'partials.privacy_policy')->name('legal.privacy_policy');
 
-Route::get('/legal/terms_conditions', function(){
-    return view('partials.terms_conditions');
-})->name('legal.terms_conditions');
+Route::view('/terms_conditions', 'partials.terms_conditions')->name('legal.terms_conditions');
 
 Route::get('/cars/filter', [CarController::class, 'filter'])
 ->name('cars.filter');
