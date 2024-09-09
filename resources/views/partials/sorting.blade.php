963a294e5e100ca4ddb1fa1261919d3dc935b08a
Cmilja <dsmiljanic40@gmail.com>
2024-09-10 00:55:11 +0200

    Search is now active while sorting

--- resources/views/partials/sorting.blade.php
+++ resources/views/partials/sorting.blade.php
@@ -10,12 +10,12 @@
 
 @if ($cars->count())
     <form action="{{route('cars.filter')}}">
-        <input type="hidden" value="{{request('brand')}}">
-        <input type="hidden" value="{{request('price')}}">
-        <input type="hidden" value="{{request('fuel')}}">
-        <input type="hidden" value="{{request('year_from')}}">
-        <input type="hidden" value="{{request('year_to')}}">
-        <input type="hidden" value="{{request('body_type')}}">
+        <input type="hidden" value="{{request('brand')}}" name="brand">
+        <input type="hidden" value="{{request('price')}}" name="price">
+        <input type="hidden" value="{{request('fuel')}}" name="fuel">
+        <input type="hidden" value="{{request('year_from')}}" name="year_from">
+        <input type="hidden" value="{{request('year_to')}}" name="year_to">
+        <input type="hidden" value="{{request('body_type')}}" name="body_type">
         <label for="sort" class="form-label">Sort by:</label>
         <select name="sort" id="sort" class="form-select sort" onchange="this.form.submit()">
             @foreach ($sorting as $sorting_key => $label)
