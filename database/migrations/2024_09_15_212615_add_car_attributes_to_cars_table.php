72a5ad985c937c9f954733ced6357032f0ca5aee
Cmilja <dsmiljanic40@gmail.com>
2024-09-15 23:41:01 +0200

    Updated car attributes

---
+++ database/migrations/2024_09_15_212615_add_car_attributes_to_cars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->string('transmission');
+            $table->integer('power');
+            $table->string('drive_system');
+            $table->integer('door_count');
+            $table->integer('number_of_seats');
+            $table->integer('cubic_capacity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->dropColumn(['transmission, power, drive_system, door_count, number_of_seats, cubic_capacity']);
+        });
+    }
+};
