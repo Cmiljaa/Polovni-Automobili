c91a3da008c0328785446fb5a92b87e8ea9611f3
Cmilja <dsmiljanic40@gmail.com>
2024-09-15 23:55:44 +0200

    Database seeding updated

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -19,17 +19,19 @@ class CarFactory extends Factory
     {
         return [
             'user_id' => User::factory(),
-            'brand' => fake()->randomElement([
-                'Audi', 'BMW', 'Mercedes-Benz', 'Toyota', 'Honda', 'Ford', 'Chevrolet', 'Nissan', 'Volkswagen', 'Hyundai',
-                'Kia', 'Subaru', 'Mazda', 'Porsche', 'Jaguar', 'Land Rover', 'Lexus', 'Volvo', 'Buick', 'Chrysler'
-            ]
-            ),
+            'brand' => fake()->randomElement(config('brands')),
             'model' => fake()->word(),
             'price' => fake()->numberBetween(5000, 100000),
             'mileage' => fake()->numberBetween(25000, 300000),
-            'fuel' => fake()->randomElement(['Diesel', 'Petrol', 'Electric', 'Hybrid']),
+            'fuel' => fake()->randomElement(config('fuels')),
             'year' => fake()->year(),
-            'body_type' => fake()->randomElement(['Crossover', 'Convertible', 'Coupe', 'Hatchback', 'Sedan', 'Sports_car', 'Suv', 'Truck', 'Van', 'Wagon']),
+            'body_type' => fake()->randomElement(config('body_types')),
+            'transmission' => fake()->randomElement(['Automatic', 'Manual', 'Semi-automatic']),
+            'power' => fake()->numberBetween(100, 350),
+            'drive_system' => fake()->randomElement(['Front-Wheel', 'Rear-Wheel', 'All-Wheel', 'Four-Wheel']),
+            'door_count' => fake()->numberBetween(2, 5),
+            'number_of_seats' => fake()->numberBetween(2, 6),
+            'cubic_capacity' => fake()->numberBetween(1000, 4000),
             'allowed' => true
         ];
     }
