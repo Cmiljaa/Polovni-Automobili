8807e4cf29b6566628b8a6cc148507cf7851e473
Cmilja <dsmiljanic40@gmail.com>
2024-09-20 19:32:52 +0200

    Navbar updated, larger items and responsive

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,45 +1,47 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{route('cars.index')}}">Polovni Automobili</a>
+        <a class="navbar-brand d-flex align-items-center" href="{{ route('cars.index') }}">
+            <span class="fs-3 fw-bold text-dark">Polovni Automobili</span>
+        </a>        
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse mt-3" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-lg-0"></ul>
 
             <ul class="navbar-nav ms-auto">
                 @if (Auth::check() && Auth::user()->is_admin)
-                    <li class="nav-item">
-                        <a href="{{route('admin.dashboard')}}" class="btn btn-primary me-2 m-1">Dashboard</a>
+                    <li class="nav-item mx-md-2 mb-2">
+                        <a href="{{route('admin.dashboard')}}" class="btn btn-primary px-3 py-2 shadow-sm w-100">Dashboard</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mx-md-2 mb-2">
                         <form action="{{route('user.logout')}}" method="POST">
                             @csrf
-                            <button class="btn btn-danger m-1" type="submit">Logout</button>
+                            <button class="btn btn-danger px-3 py-2 shadow-sm w-100" type="submit">Logout</button>
                         </form>
                     </li>
                 @elseif (Auth::check())
-                    <li class="nav-item">
-                        <a href="{{route('user.edit', Auth::id())}}" class="btn btn-info me-2 m-1">Your Profile</a>
+                    <li class="nav-item mx-md-2 mb-2">
+                        <a href="{{route('user.edit', Auth::id())}}" class="btn btn-secondary px-3 py-2 shadow-sm w-100">Your Profile</a>
                     </li>
-                    <li class="nav-item">
-                        <a href="{{route('user.list')}}" class="btn btn-primary me-2 m-1">Your Cars</a>
+                    <li class="nav-item mx-md-2 mb-2">
+                        <a href="{{route('user.list')}}" class="btn btn-primary px-3 py-2 shadow-sm w-100">Your Cars</a>
                     </li>
-                    <li class="nav-item">
-                        <a href="{{route('cars.create')}}" class="btn btn-warning me-2 m-1">Add New Car</a>
+                    <li class="nav-item mx-md-2 mb-2">
+                        <a href="{{route('cars.create')}}" class="btn btn-warning px-3 py-2 shadow-sm w-100">Add New Car</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mx-md-2 mb-2">
                         <form action="{{route('user.logout')}}" method="POST">
                             @csrf
-                            <button class="btn btn-danger m-1" type="submit">Logout</button>
+                            <button class="btn btn-danger px-3 py-2 shadow-sm w-100" type="submit">Logout</button>
                         </form>
                     </li>
                 @else
-                    <li class="nav-item">
-                        <a href="{{route('login')}}" class="btn btn-success me-2 m-1">Login</a>
+                    <li class="nav-item mx-md-2 mb-2">
+                        <a href="{{route('login')}}" class="btn btn-success px-3 py-2 shadow-sm w-100">Login</a>
                     </li>
-                    <li class="nav-item">
-                        <a href="{{route('user.create')}}" class="btn btn-primary m-1">Register</a>
+                    <li class="nav-item mx-md-2 mb-2">
+                        <a href="{{route('user.create')}}" class="btn btn-primary px-3 py-2 shadow-sm w-100">Register</a>
                     </li>
                 @endif
             </ul>
