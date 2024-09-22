2068cbf5f10eef7e744934c96e305c992e1ea548
Cmilja <dsmiljanic40@gmail.com>
2024-09-22 22:31:16 +0200

    Sorting conf file

---
+++ config/car/sorting.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'created_at,desc' => 'Latest',
+    'price,asc' => 'Price: Low to High',
+    'price,desc' => 'Price: High to Low',
+    'year,asc' => 'Year: Old to New',
+    'year,desc' => 'Year: New to Old',
+    'mileage,asc' => 'Mileage: Low to High',
+    'mileage,desc' => 'Mileage: High to Low',
+];
--- resources/views/partials/sorting.blade.php
+++ resources/views/partials/sorting.blade.php
@@ -1,13 +1,3 @@
-@php
-    $sorting = [
-        'created_at,desc' => 'Latest',
-        'price,asc' => 'Price: Low to High',
-        'price,desc' => 'Price: High to Low',
-        'year,asc' => 'Year: Old to New',
-        'year,desc' => 'Year: New to Old'
-    ];
-@endphp
-
 @if ($cars->count())
     <form action="{{route('cars.filter')}}">
         <input type="hidden" value="{{request('brand')}}" name="brand">
@@ -18,7 +8,7 @@
         <input type="hidden" value="{{request('body_type')}}" name="body_type">
         <label for="sort" class="form-label">Sort by:</label>
         <select name="sort" id="sort" class="form-select sort" onchange="this.form.submit()">
-            @foreach ($sorting as $sorting_key => $label)
+            @foreach (config('car.sorting') as $sorting_key => $label)
                 <option value="{{$sorting_key}}"  {{ (isset($selectedSort) && $selectedSort == $sorting_key) || old('sort') == $sorting_key ? 'selected' : '' }}>{{$label}}</option>
             @endforeach
         </select>
