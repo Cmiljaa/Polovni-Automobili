60dd05d6a710825fcb7377ac9706cbaa908f31db
Cmilja <dsmiljanic40@gmail.com>
2024-09-30 20:28:35 +0200

    Basic Application Tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,6 +7,33 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    //Basic Application Tests
+
+    public function test_the_application_redirects_to_cars(): void
+    {
+        $response = $this->get('/');
+        $response->assertRedirect('/cars');
+    }
+
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
+
+    public function test_privacy_policy_page_is_accessible(): void
+    {
+        $response = $this->get('/privacy_policy');
+        $response->assertStatus(200);
+        $response->assertSee('Privacy Policy');
+    }
+
+    public function test_terms_and_conditions_page_is_accessible(): void
+    {
+        $response = $this->get('/terms_conditions');
+        $response->assertStatus(200);
+        $response->assertSee('Privacy Policy');
+    }
     /**
      * A basic test example.
      */
